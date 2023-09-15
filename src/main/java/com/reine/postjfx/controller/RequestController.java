448e3fb7417c502c23d0a704c4b5c3086d1d7fdd
reine-ishyanami <2402979195@qq.com>
2023-09-15 10:54:38 +0800

    添加或减少请求标头表格行

--- src/main/java/com/reine/postjfx/controller/RequestController.java
+++ src/main/java/com/reine/postjfx/controller/RequestController.java
@@ -7,14 +7,15 @@ import com.reine.postjfx.enums.RequestMethodEnum;
 import com.reine.postjfx.utils.HttpUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.ChoiceBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 import javafx.util.StringConverter;
 
 import java.io.IOException;
@@ -28,12 +29,7 @@ public class RequestController extends VBox {
     private TextField urlField;
 
     @FXML
-    private TableView<HeaderProperty> headerTableView;
-
-    @FXML
-    private TableColumn<HeaderProperty, HeaderTypeEnum> nameColOfHeaderTableView;
-    @FXML
-    private TableColumn<HeaderProperty, String> valueColOfHeaderTableView;
+    private TableView<HeaderProperty> headersTableView;
 
     @FXML
     private TableView<?> paramsTableView;
@@ -64,8 +60,31 @@ public class RequestController extends VBox {
 
     @FXML
     void initialize() {
-        nameColOfHeaderTableView.setCellValueFactory(new PropertyValueFactory<>("headerTypeEnum"));
-        nameColOfHeaderTableView.setCellFactory(ChoiceBoxTableCell.forTableColumn(new StringConverter<>() {
+        initHeadersTableView();
+        initParamsTableView();
+        initTopRequestNode();
+    }
+
+    @FXML
+    private TableColumn<HeaderProperty, HeaderTypeEnum> nameColOfHeadersTableView;
+
+    @FXML
+    private TableColumn<HeaderProperty, String> valueColOfHeadersTableView;
+
+    @FXML
+    private TableColumn<HeaderProperty, Void> addRowOfHeadersTableView;
+
+    @FXML
+    private TableColumn<HeaderProperty, Void> removeRowOfHeadersTableView;
+
+    /**
+     * 初始化请求标头表
+     */
+    void initHeadersTableView() {
+        headersTableView.setColumnResizePolicy(param -> true);
+        headersTableView.getItems().addAll(new HeaderProperty(HeaderTypeEnum.CONTENT_TYPE, "application/json"));
+        nameColOfHeadersTableView.setCellValueFactory(new PropertyValueFactory<>("headerTypeEnum"));
+        nameColOfHeadersTableView.setCellFactory(ChoiceBoxTableCell.forTableColumn(new StringConverter<>() {
             @Override
             public String toString(HeaderTypeEnum object) {
                 return object != null ? object.getName() : "";
@@ -76,11 +95,71 @@ public class RequestController extends VBox {
                 return HeaderTypeEnum.valueOf(string);
             }
         }, HeaderTypeEnum.values()));
-        valueColOfHeaderTableView.setCellValueFactory(new PropertyValueFactory<>("value"));
-        valueColOfHeaderTableView.setCellFactory(TextFieldTableCell.forTableColumn());
-        headerTableView.setColumnResizePolicy(param -> true);
-        headerTableView.getItems().addAll(new HeaderProperty(HeaderTypeEnum.CONTENT_TYPE, "application/json"));
+        valueColOfHeadersTableView.setCellValueFactory(new PropertyValueFactory<>("value"));
+        valueColOfHeadersTableView.setCellFactory(TextFieldTableCell.forTableColumn());
+        addRowOfHeadersTableView.setCellFactory(new Callback<>() {
+            @Override
+            public TableCell<HeaderProperty, Void> call(TableColumn<HeaderProperty, Void> param) {
+                return new TableCell<>() {
+                    @Override
+                    protected void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (!empty) {
+                            HBox root = new HBox();
+                            root.setAlignment(Pos.CENTER);
+                            ImageView imageView = new ImageView("/image/add.png");
+                            imageView.setFitWidth(10);
+                            imageView.setFitHeight(10);
+                            root.getChildren().add(imageView);
+                            this.setGraphic(root);
+                            root.setOnMouseClicked(event -> headersTableView.getItems().add(new HeaderProperty()));
+                        } else {
+                            this.setGraphic(null);
+                        }
+                    }
+                };
+            }
+        });
+        removeRowOfHeadersTableView.setCellFactory(new Callback<>() {
+            @Override
+            public TableCell<HeaderProperty, Void> call(TableColumn<HeaderProperty, Void> param) {
+                return new TableCell<>() {
+                    @Override
+                    protected void updateItem(Void item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (!empty) {
+                            HBox root = new HBox();
+                            root.setAlignment(Pos.CENTER);
+                            ImageView imageView = new ImageView("/image/sub.png");
+                            imageView.setFitWidth(10);
+                            imageView.setFitHeight(10);
+                            root.getChildren().add(imageView);
+                            this.setGraphic(root);
+                            root.setOnMouseClicked(event -> {
+                                if (headersTableView.getItems().size() > 1)
+                                    headersTableView.getItems().remove(this.getIndex());
+                            });
+                        } else {
+                            this.setGraphic(null);
+                        }
+                    }
+                };
+            }
+        });
+
+    }
+
+    /**
+     * 初始化请求参数表
+     */
+    void initParamsTableView() {
         paramsTableView.setColumnResizePolicy(param -> true);
+    }
+
+    /**
+     * 初始化顶部请求操作组件
+     */
+    void initTopRequestNode() {
         methodChoiceBox.getItems().addAll(RequestMethodEnum.GET, RequestMethodEnum.POST, RequestMethodEnum.PUT, RequestMethodEnum.DELETE);
         methodChoiceBox.setValue(RequestMethodEnum.GET);
         methodChoiceBox.setConverter(new StringConverter<>() {
@@ -94,7 +173,6 @@ public class RequestController extends VBox {
                 return null;
             }
         });
-
     }
 
 }
--- src/main/java/com/reine/postjfx/controller/ResponseController.java
+++ src/main/java/com/reine/postjfx/controller/ResponseController.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 
 import java.io.IOException;
 import java.net.http.HttpResponse;
