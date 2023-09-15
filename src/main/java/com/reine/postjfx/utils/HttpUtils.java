6726ef79e11278d2c5324a174ede30451adf49b7
reine-ishyanami <2402979195@qq.com>
2023-09-15 13:19:46 +0800

    携带请求参数发送请求

--- src/main/java/com/reine/postjfx/utils/HttpUtils.java
+++ src/main/java/com/reine/postjfx/utils/HttpUtils.java
@@ -1,6 +1,7 @@
 package com.reine.postjfx.utils;
 
 import com.reine.postjfx.entity.HeaderProperty;
+import com.reine.postjfx.entity.ParamProperty;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,7 +9,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Stream;
 
@@ -30,24 +30,9 @@ public class HttpUtils {
      * @param headers
      * @return
      */
-    public static CompletableFuture<HttpResponse<String>> get(String url, Map<String, Object> params, List<HeaderProperty> headers) {
-        StringBuilder queryString = new StringBuilder();
-        if (params != null) {
-            for (Map.Entry<String, Object> entry : params.entrySet()) {
-                if (!queryString.isEmpty()) queryString.append("&");
-                queryString.append(entry.getKey()).append("=").append(entry.getValue());
-            }
-        }
-        String[] headerArray;
-        if (headers != null) {
-            headerArray = headers.stream().map(headerProperty -> {
-                String first = headerProperty.getHeaderTypeEnum().getName();
-                String second = headerProperty.getValue();
-                return new String[]{first, second};
-            }).reduce((a, b) -> Stream.concat(Arrays.stream(a), Arrays.stream(b)).toArray(String[]::new)).orElse(new String[]{});
-        } else {
-            headerArray = defaultHeader;
-        }
+    public static CompletableFuture<HttpResponse<String>> get(String url, List<ParamProperty> params, List<HeaderProperty> headers) {
+        String queryString = handleQueryParam(params);
+        String[] headerArray = handleRequestHeader(headers);
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(String.format("%s?%s", url, queryString)))
                 .headers(headerArray)
@@ -55,4 +40,42 @@ public class HttpUtils {
                 .build();
         return client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
     }
+
+    /**
+     * 解析请求参数
+     *
+     * @param params
+     * @return
+     */
+    private static String handleQueryParam(List<ParamProperty> params) {
+        if (params != null)
+            return params.stream().map(paramProperty -> {
+                String key = paramProperty.getKey();
+                String value = paramProperty.getValue();
+                return String.format("%s=%s", key, value);
+            }).reduce((a, b) -> String.format("%s&%s", a, b)).orElse("");
+        else return "";
+    }
+
+    /**
+     * 解析请求头
+     *
+     * @param headers
+     * @return
+     */
+    private static String[] handleRequestHeader(List<HeaderProperty> headers) {
+        if (headers != null)
+            return
+                    headers.stream()
+                            .map(headerProperty -> {
+                                String first = headerProperty.getHeaderTypeEnum().getName();
+                                String second = headerProperty.getValue();
+                                return new String[]{first, second};
+                            })
+                            .reduce((a, b) -> Stream.concat(Arrays.stream(a), Arrays.stream(b)).toArray(String[]::new))
+                            .orElse(new String[]{});
+        else return defaultHeader;
+    }
+
+
 }
