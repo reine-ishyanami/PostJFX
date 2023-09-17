9a36062367834551729663d3da5a634d6a7726fb
reine-ishyanami <2402979195@qq.com>
2023-09-17 10:49:41 +0800

    添加图标

--- src/main/java/com/reine/postjfx/App.java
+++ src/main/java/com/reine/postjfx/App.java
@@ -3,6 +3,7 @@ package com.reine.postjfx;
 import com.reine.postjfx.controller.MainController;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class App extends Application {
         Scene scene = new Scene(mainController);
         stage.setTitle("PostJFX");
         stage.setScene(scene);
+        stage.getIcons().add(new Image("/image/logo.png"));
         stage.setResizable(false);
         stage.show();
     }
