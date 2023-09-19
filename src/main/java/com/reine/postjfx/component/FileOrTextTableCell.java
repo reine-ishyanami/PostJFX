d488ecdcb0a66c0eb55f446ce93fff1eb4e5c8fb
reine-ishyanami <2402979195@qq.com>
2023-09-19 12:51:25 +0800

    取消编辑

--- src/main/java/com/reine/postjfx/component/FileOrTextTableCell.java
+++ src/main/java/com/reine/postjfx/component/FileOrTextTableCell.java
@@ -1,14 +1,17 @@
 package com.reine.postjfx.component;
 
 import com.reine.postjfx.entity.ParamProperty;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.stage.FileChooser;
 
 import java.util.Optional;
 
 /**
  * 文本与文件输入切换
+ *
  * @author reine
  */
 public class FileOrTextTableCell extends TableCell<ParamProperty, String> {
@@ -17,12 +20,15 @@ public class FileOrTextTableCell extends TableCell<ParamProperty, String> {
 
     public FileOrTextTableCell() {
         valueTextField.setOnAction(event -> commitEdit(valueTextField.getText()));
+        valueTextField.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.ESCAPE)) cancelEdit();
+        });
     }
 
     @Override
     public void startEdit() {
         super.startEdit();
-        setText(null);
+        setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
         ParamProperty row = getTableRow().getItem();
         switch (row.getParamTypeEnum()) {
             case TEXT -> {
@@ -48,8 +54,16 @@ public class FileOrTextTableCell extends TableCell<ParamProperty, String> {
     @Override
     public void commitEdit(String newValue) {
         super.commitEdit(newValue);
+        setContentDisplay(ContentDisplay.TEXT_ONLY);
         setGraphic(null);
         setText(newValue);
         getTableView().getItems().get(getIndex()).setValue(newValue);
+    }
+
+    @Override
+    public void cancelEdit() {
+        super.cancelEdit();
+        setContentDisplay(ContentDisplay.TEXT_ONLY);
+        setGraphic(null);
     }
 }
