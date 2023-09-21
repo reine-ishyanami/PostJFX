cd126256d76e20e3895d51a5cafc047695f9612b
reine-ishyanami <2402979195@qq.com>
2023-09-21 11:26:07 +0800

    选项卡处理逻辑优化

--- src/main/java/com/reine/postjfx/controller/PostTabController.java
+++ src/main/java/com/reine/postjfx/controller/PostTabController.java
@@ -3,14 +3,18 @@ package com.reine.postjfx.controller;
 import com.reine.postjfx.App;
 import com.reine.postjfx.component.EditableTab;
 import javafx.collections.ObservableList;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 
 import java.io.IOException;
 
 /**
+ * 多选项卡
+ *
  * @author reine
  */
 public class PostTabController extends TabPane {
@@ -19,7 +23,7 @@ public class PostTabController extends TabPane {
     private Tab addPostPageTab;
 
     @FXML
-    private EditableTab initTab;
+    private Button addTabButton;
 
     public PostTabController() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("post-tab.fxml"));
@@ -35,7 +39,8 @@ public class PostTabController extends TabPane {
     void addPostPage() {
         try {
             EditableTab tab = new EditableTab();
-            tab.setTitle("New Tab");
+            tab.setTitle("新标签页");
+            tab.setOnCloseRequest(this::onCloseRequestCheck);
             tab.setContent(new PostPageController());
             ObservableList<Tab> tabs = this.getTabs();
             Tab addTabButton = tabs.remove(tabs.size() - 1);
@@ -47,7 +52,16 @@ public class PostTabController extends TabPane {
 
     @FXML
     void initialize() {
-        initTab.setTitle("New Tab");
         this.setStyle("-fx-open-tab-animation: GROW; -fx-close-tab-animation: GROW;");
+        // 默认触发一次按钮，新建一个标签页
+        addTabButton.fire();
+    }
+
+    /**
+     * 检查该选项卡是否是最后一个选项卡，如果是，则不能关闭
+     */
+    @FXML
+    void onCloseRequestCheck(Event event) {
+        if (this.getTabs().size() <= 2) event.consume();
     }
 }
