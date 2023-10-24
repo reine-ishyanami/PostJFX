a85c7333745fbaf2833f1b77f8045853ddb0e2f7
reine-ishyanami <2402979195@qq.com>
2023-10-24 12:28:31 +0800

    优化代码结构

--- src/main/java/com/reine/postjfx/utils/LogUtils.java
+++ src/main/java/com/reine/postjfx/utils/LogUtils.java
@@ -1,13 +1,31 @@
 package com.reine.postjfx.utils;
 
 import com.reine.postjfx.entity.Log;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.util.ArrayList;
+
 /**
+ * 日志操作工具类
  * @author reine
  */
 public class LogUtils {
 
-    public static ObservableList<Log> logList;
+    public final static ObservableList<Log> logList = FXCollections.observableList(new ArrayList<>());
+
+    /**
+     * 保存日志信息到文件
+     */
+    public static void saveLogListToFile(){
+
+    }
+
+    /**
+     * 读取文件信息写入日志列表
+     */
+    public static void readFromFileForLogList(){
+
+    }
 
 }
