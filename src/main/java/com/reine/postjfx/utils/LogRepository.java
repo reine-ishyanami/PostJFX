74fc35b00881c2c760db24b786e2f814f43bb256
reine-ishyanami <2402979195@qq.com>
2023-11-01 11:48:49 +0800

    封装日志表操作

---
+++ src/main/java/com/reine/postjfx/utils/LogRepository.java
@@ -0,0 +1,200 @@
+package com.reine.postjfx.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.reine.postjfx.entity.HeaderProperty;
+import com.reine.postjfx.entity.Log;
+import com.reine.postjfx.entity.ParamProperty;
+import javafx.collections.FXCollections;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 日志数据表操作类
+ *
+ * @author reine
+ */
+public class LogRepository {
+
+    /**
+     * jackson 数据转换
+     */
+    private final static ObjectMapper mapper = new ObjectMapper();
+
+    /**
+     * 数据库连接
+     */
+    private static final Connection connection;
+
+
+    static {
+        try {
+            // 创建文件夹
+            Path dir = Constant.logDbDir;
+            Files.createDirectories(dir);
+            // 加载数据库驱动
+            Class.forName("org.sqlite.JDBC");
+            // 建立数据库连接
+            connection = DriverManager.getConnection(String.format("jdbc:sqlite:%s/log.db", dir));
+        } catch (SQLException | IOException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 建表语句
+     */
+    private static final String tableSql = """
+            CREATE TABLE IF NOT EXISTS log
+            (
+                datetime TEXT,
+                method   TEXT,
+                params   TEXT,
+                headers  TEXT,
+                body     TEXT,
+                url      TEXT
+            );
+            """;
+
+    /**
+     * 创建数据库
+     */
+    public static void createTable() {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(tableSql);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 建索引语句
+     */
+    private static final String indexSql = """
+            CREATE INDEX IF NOT EXISTS datetime_index ON log(datetime);
+            """;
+
+
+    /**
+     * 创建索引
+     */
+    public static void createIndex() {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(indexSql);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 插入语句
+     */
+    private static final String insertSql = """
+            INSERT INTO log('datetime', 'method','url', 'params','headers', 'body' )
+            VALUES (?, ?, ?, ?, ?, ?);
+            """;
+
+
+    /**
+     * 往数据库中插入一条数据
+     */
+    public static void insertOne(Log log) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(insertSql);
+            preparedStatement.setString(1, log.dateTime());
+            preparedStatement.setString(2, log.method());
+            preparedStatement.setString(3, log.url());
+            preparedStatement.setString(4, mapper.writeValueAsString(log.params()));
+            preparedStatement.setString(5, mapper.writeValueAsString(log.headers()));
+            preparedStatement.setString(6, log.body());
+            preparedStatement.execute();
+        } catch (SQLException | JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 删除语句
+     */
+    private static final String deleteSql = """
+            DELETE FROM log WHERE datetime = ?
+            """;
+
+    /**
+     * 删除数据库中指定一条数据
+     */
+    public static void removeOne(Log log) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(deleteSql);
+            preparedStatement.setString(1, log.dateTime());
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 日期格式化器
+     */
+    private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+    /**
+     * 查询语句
+     */
+    private static final String querySql = """
+            SELECT datetime, method, url, params, headers, body
+            FROM log
+            WHERE substr(datetime, 1, instr(datetime, '_') - 1) = ?
+            ORDER BY datetime
+            """;
+
+    /**
+     * 从数据库中查询指定数据
+     */
+    public static List<Log> selectListByDate(LocalDate date) {
+        try {
+            String dateStr = date.format(formatter);
+            PreparedStatement preparedStatement = connection.prepareStatement(querySql);
+            preparedStatement.setString(1, dateStr);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Log> logList = new ArrayList<>();
+            while (resultSet.next()) {
+                String datetime = resultSet.getString("datetime");
+                String method = resultSet.getString("method");
+                String url = resultSet.getString("url");
+                String paramsStr = resultSet.getString("params");
+                List<ParamProperty> params = mapper.readValue(paramsStr, new TypeReference<>() {
+                });
+                String headersStr = resultSet.getString("headers");
+                List<HeaderProperty> headers = mapper.readValue(headersStr, new TypeReference<>() {
+                });
+                String body = resultSet.getString("body");
+                Log log = new Log(datetime,
+                        method,
+                        url,
+                        FXCollections.observableList(params),
+                        FXCollections.observableList(headers),
+                        body);
+                // 添加到日志列表中
+                logList.add(log);
+            }
+            return logList;
+        } catch (SQLException | JsonProcessingException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/reine/postjfx/utils/LogUtils.java
+++ src/main/java/com/reine/postjfx/utils/LogUtils.java
@@ -1,23 +1,13 @@
 package com.reine.postjfx.utils;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.reine.postjfx.entity.HeaderProperty;
 import com.reine.postjfx.entity.Log;
-import com.reine.postjfx.entity.ParamProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.sql.*;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
@@ -29,16 +19,6 @@ import java.util.List;
  * @author reine
  */
 public class LogUtils {
-
-    /**
-     * 日期格式化器
-     */
-    private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-
-    /**
-     * jackson 数据转换
-     */
-    private final static ObjectMapper mapper = new ObjectMapper();
 
     /**
      * 右侧历史记录数据
@@ -57,11 +37,6 @@ public class LogUtils {
     private static boolean initializing = false;
 
     /**
-     * 数据库连接
-     */
-    private static final Connection connection;
-
-    /**
      * 被删除的日志栈
      */
     private static final Deque<Log> logStack = new ArrayDeque<>();
@@ -71,103 +46,30 @@ public class LogUtils {
      */
     public static final SimpleIntegerProperty logStackSize = new SimpleIntegerProperty(0);
 
-    /**
-     * 建表语句
-     */
-    private static final String tableSql = """
-            CREATE TABLE IF NOT EXISTS log
-            (
-                datetime TEXT,
-                method   TEXT,
-                params   TEXT,
-                headers  TEXT,
-                body     TEXT,
-                url      TEXT
-            );
-            """;
-
-    /**
-     * 建索引语句
-     */
-    private static final String indexSql = """
-            CREATE INDEX IF NOT EXISTS datetime_index ON log(datetime);
-            """;
-
-    /**
-     * 插入语句
-     */
-    private static final String insertSql = """
-            INSERT INTO log('datetime', 'method','url', 'params','headers', 'body' )
-            VALUES (?, ?, ?, ?, ?, ?);
-            """;
-
-    /**
-     * 删除语句
-     */
-    private static final String deleteSql = """
-            DELETE FROM log WHERE datetime = ?
-            """;
-
     static {
-        try {
-            // 创建文件夹
-            Path dir = Constant.logDbDir;
-            Files.createDirectories(dir);
-            // 加载数据库驱动
-            Class.forName("org.sqlite.JDBC");
-            // 建立数据库连接
-            connection = DriverManager.getConnection(String.format("jdbc:sqlite:%s/log.db", dir));
-            PreparedStatement preparedStatement = connection.prepareStatement(tableSql);
-            preparedStatement.execute();
-            preparedStatement = connection.prepareStatement(indexSql);
-            preparedStatement.execute();
-        } catch (ClassNotFoundException | SQLException | IOException e) {
-            throw new RuntimeException(e);
-        }
+        LogRepository.createTable();
+        LogRepository.createIndex();
         // 保存日志信息到数据库
         logList.addListener((ListChangeListener<Log>) c -> {
             // 如果是应用启动时的初始化数据操作，则不进行数据列表的响应操作
             if (!initializing)
-                try {
-                    while (c.next()) {
-                        // 当请求发送成功时，往日志数据库中添加一条数据
-                        if (c.wasAdded()) {
-                            Log log = c.getAddedSubList().get(0);
-                            // 向数据库中写入一条数据
-                            PreparedStatement preparedStatement = connection.prepareStatement(insertSql);
-                            preparedStatement.setString(1, log.dateTime());
-                            preparedStatement.setString(2, log.method());
-                            preparedStatement.setString(3, log.url());
-                            preparedStatement.setString(4, mapper.writeValueAsString(log.params()));
-                            preparedStatement.setString(5, mapper.writeValueAsString(log.headers()));
-                            preparedStatement.setString(6, log.body());
-                            preparedStatement.execute();
-                        }
-                        // 点击日志项的删除按钮时，删除数据库中的日志信息
-                        if (c.wasRemoved()) {
-                            Log log = c.getRemoved().get(0);
-                            // 删除数据库中指定日期时间的数据
-                            PreparedStatement preparedStatement = connection.prepareStatement(deleteSql);
-                            preparedStatement.setString(1, log.dateTime());
-                            preparedStatement.execute();
-                            push(log);
-                        }
+                while (c.next()) {
+                    // 当请求发送成功时，往日志数据库中添加一条数据
+                    if (c.wasAdded()) {
+                        Log log = c.getAddedSubList().get(0);
+                        // 向数据库中写入一条数据
+                        LogRepository.insertOne(log);
                     }
-                } catch (SQLException | JsonProcessingException e) {
-                    throw new RuntimeException(e);
+                    // 点击日志项的删除按钮时，删除数据库中的日志信息
+                    if (c.wasRemoved()) {
+                        Log log = c.getRemoved().get(0);
+                        // 删除数据库中指定日期时间的数据
+                        LogRepository.removeOne(log);
+                        push(log);
+                    }
                 }
         });
     }
-
-    /**
-     * 查询语句
-     */
-    private static final String querySql = """
-            SELECT datetime, method, url, params, headers, body
-            FROM log
-            WHERE substr(datetime, 1, instr(datetime, '_') - 1) = ?
-            ORDER BY datetime
-            """;
 
     /**
      * 根据传入的日期查询当日日志信息
@@ -175,40 +77,12 @@ public class LogUtils {
     public static void readFromFileForLogList(LocalDate date) {
         // 设置为正在初始化数据
         initializing = true;
-        String dateStr = date.format(formatter);
         // 清空数据，等待重新装填
         logList.clear();
-        try {
-            // 查询当天的所有请求操作
-            PreparedStatement preparedStatement = connection.prepareStatement(querySql);
-            preparedStatement.setString(1, dateStr);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                String datetime = resultSet.getString("datetime");
-                String method = resultSet.getString("method");
-                String url = resultSet.getString("url");
-                String paramsStr = resultSet.getString("params");
-                List<ParamProperty> params = mapper.readValue(paramsStr, new TypeReference<>() {
-                });
-                String headersStr = resultSet.getString("headers");
-                List<HeaderProperty> headers = mapper.readValue(headersStr, new TypeReference<>() {
-                });
-                String body = resultSet.getString("body");
-                Log log = new Log(datetime,
-                        method,
-                        url,
-                        FXCollections.observableList(params),
-                        FXCollections.observableList(headers),
-                        body);
-                // 添加到日志列表中
-                logList.add(log);
-            }
-        } catch (SQLException | JsonProcessingException e) {
-            throw new RuntimeException(e);
-        } finally {
-            // 初始化完成，将初始化标志字段取反
-            initializing = false;
-        }
+        List<Log> logs = LogRepository.selectListByDate(date);
+        logList.addAll(logs);
+        // 初始化完成，将初始化标志字段取反
+        initializing = false;
     }
 
     /**
@@ -216,19 +90,8 @@ public class LogUtils {
      */
     public static void restoreHistory(LocalDate date) {
         // 从栈中弹出一条最近的删除记录
-        try {
-            Log recentLog = pop();
-            PreparedStatement preparedStatement = connection.prepareStatement(insertSql);
-            preparedStatement.setString(1, recentLog.dateTime());
-            preparedStatement.setString(2, recentLog.method());
-            preparedStatement.setString(3, recentLog.url());
-            preparedStatement.setString(4, mapper.writeValueAsString(recentLog.params()));
-            preparedStatement.setString(5, mapper.writeValueAsString(recentLog.headers()));
-            preparedStatement.setString(6, recentLog.body());
-            preparedStatement.execute();
-        } catch (SQLException | JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        Log recentLog = pop();
+        LogRepository.removeOne(recentLog);
         // 重新加载数据
         readFromFileForLogList(date);
     }
