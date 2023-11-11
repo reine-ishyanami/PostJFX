a96db7aeb6bd81797c6334d631e19661775043bd
reine-ishyanami <2402979195@qq.com>
2023-11-11 16:00:55 +0800

    修改标签页样式

--- src/main/java/com/reine/postjfx/controller/PostTabController.java
+++ src/main/java/com/reine/postjfx/controller/PostTabController.java
@@ -7,11 +7,17 @@ import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.control.skin.TabPaneSkin;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.*;
 
 /**
  * 多选项卡
@@ -45,6 +51,8 @@ public class PostTabController extends TabPane {
             // 设置新标签页内容
             tab.setContent(new PostPageController());
             insertNewTab(tab);
+            // 每次点击按钮，清空缓存
+            if (!errorX.isEmpty()) errorX.clear();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -71,15 +79,24 @@ public class PostTabController extends TabPane {
 
     /**
      * 插入新标签页
+     *
      * @param tab 要增加的新标签页
      */
     private void insertNewTab(EditableTab tab) {
         ObservableList<Tab> tabs = this.getTabs();
-        Tab addTabButton = tabs.remove(tabs.size() - 1);
-        tabs.addAll(tab, addTabButton);
-        // 如果标签页数量过多，则移除添加按钮
-        if (getWidth() > 800) tabs.remove(btnTab);
+        if (tabs.get(tabs.size() - 1).equals(btnTab)) tabs.add(tabs.size() - 1, tab);
+        else tabs.add(tab);
+        double maxX = addTabButton.localToScene(addTabButton.getBoundsInLocal()).getMaxX();
+        if (maxX > 750.0) {
+            errorX.add(maxX);
+            tabs.remove(btnTab);
+        }
     }
+
+    /**
+     * 缓存溢出标签页的位置信息
+     */
+    private final Set<Double> errorX = new HashSet<>();
 
 
     @FXML
@@ -96,8 +113,10 @@ public class PostTabController extends TabPane {
     @FXML
     void onCloseRequestCheck(Event event) {
         if (this.getTabs().size() <= 2) event.consume();
-        // 当标签栏长度没有溢出时，判断最后一个标签是否为btnTab，如果不是则添加
-        if (getWidth() <= 800.0 && !getTabs().get(getTabs().size() - 1).equals(btnTab)) getTabs().add(btnTab);
+        // 删除时默认添加增加标签页
+        if (!getTabs().get(getTabs().size() - 1).equals(btnTab)) getTabs().add(btnTab);
+        double maxX = addTabButton.localToScene(addTabButton.getBoundsInLocal()).getMaxX();
+        // 如果添加完增加标签页后，其偏移量大于指定数值，则代表此时标签页仍然是溢出状态，移除掉增加标签页的按钮
+        if (maxX > 900.0 || errorX.contains(maxX)) getTabs().remove(btnTab);
     }
-
 }
